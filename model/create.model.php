05aa049154ca49225eb8b2791248d777eff329cc
LukaszGolko <lukaszgolko26@gmail.com>
2024-07-01 22:00:49 +0200

    10 commit

--- model/create.model.php
+++ model/create.model.php
@@ -9,19 +9,17 @@ if (isset($name) && isset($lastname)) {
 
     $queryWhetherUserExists = ("SELECT name, lastName FROM users WHERE name LIKE :name AND lastName LIKE :lastname");
     $executeQueryWhetherUserExists = $db->query($queryWhetherUserExists, [':name' => $name, ':lastname' => $lastname])->fetch();
-    var_dump($executeQueryWhetherUserExists);
-
-
-
-
-    // if($executeQueryWhetherUserExists == true){
-    //     $UserExists = true;
-    // }else
-    // {
-    //     $UserExists = false;
-    //     $saveUser = "INSERT INTO users (name, lastName) VALUES (':name', ':lastname')";
-    //     $db->query($saveUser, [':name' => $name, ':lastname' => $lastname]);
-    // }
+    // var_dump($executeQueryWhetherUserExists);
+    if(is_array($executeQueryWhetherUserExists)){
+         if($executeQueryWhetherUserExists['name'] == $name && $executeQueryWhetherUserExists['lastName'] == $lastname){
+            echo "User" . " " . $executeQueryWhetherUserExists['name'] . " " . $executeQueryWhetherUserExists['lastName'] . " " . "already exists";
+         }
+    }else
+    {
+        $saveUser = "INSERT INTO users (name, lastName) VALUES (:name, :lastname)";
+        $db->query($saveUser, [':name' => $name, ':lastname' => $lastname]);
+        echo "User" . " " . $name . " " . $lastname . " " . "has been saved";
+    }
 
 }
 
