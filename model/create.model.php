1cf9e97d663f8c8728832b552d4112143a70fb35
LukaszGolko <lukaszgolko26@gmail.com>
2024-07-03 15:14:30 +0200

    Create already done

--- model/create.model.php
+++ model/create.model.php
@@ -10,15 +10,10 @@ if (isset($name) && isset($lastname)) {
     $queryWhetherUserExists = ("SELECT name, lastName FROM users WHERE name LIKE :name AND lastName LIKE :lastname");
     $executeQueryWhetherUserExists = $db->query($queryWhetherUserExists, [':name' => $name, ':lastname' => $lastname])->fetch();
     // var_dump($executeQueryWhetherUserExists);
-    if(is_array($executeQueryWhetherUserExists)){
-         if($executeQueryWhetherUserExists['name'] == $name && $executeQueryWhetherUserExists['lastName'] == $lastname){
-            echo "User" . " " . $executeQueryWhetherUserExists['name'] . " " . $executeQueryWhetherUserExists['lastName'] . " " . "already exists";
-         }
-    }else
-    {
+    
+    if($executeQueryWhetherUserExists === false){
         $saveUser = "INSERT INTO users (name, lastName) VALUES (:name, :lastname)";
         $db->query($saveUser, [':name' => $name, ':lastname' => $lastname]);
-        echo "User" . " " . $name . " " . $lastname . " " . "has been saved";
     }
 
 }
--- view/create.view.php
+++ view/create.view.php
@@ -1,13 +1,26 @@
 <?php
-    require __DIR__ . "/../view/partials/head.php";
-    require __DIR__ . "/../view/partials/banner.php";
-    require __DIR__ . "/../view/partials/nav.php";
+require __DIR__ . "/../view/partials/head.php";
+require __DIR__ . "/../view/partials/banner.php";
+require __DIR__ . "/../view/partials/nav.php";
 ?>
-    <form action="/../controller/create.controller.php" method="POST">
-        Name*:<input type="text" name="name" required><br>
-        Last name*:<input type="text" name="lastname" required><br>
-        <input type="submit" value="SUBMIT"><br>
-    </form>
+<form action="/../controller/create.controller.php" method="POST">
+    Name*:<input type="text" name="name" required><br>
+    Last name*:<input type="text" name="lastname" required><br>
+    <input type="submit" value="SUBMIT"><br>
+</form>
 <?php
-    require __DIR__ . "/../view/partials/footer.php";
+
+if (isset($name) && isset($lastname)) {
+    if(is_array($executeQueryWhetherUserExists)){
+         if($executeQueryWhetherUserExists['name'] == $name && $executeQueryWhetherUserExists['lastName'] == $lastname){
+            echo "User" . " " . $executeQueryWhetherUserExists['name'] . " " . $executeQueryWhetherUserExists['lastName'] . " " . "already exists";
+         }
+    }else
+    {
+        echo "User" . " " . $name . " " . $lastname . " " . "has been saved";
+    }
+
+}
+
+require __DIR__ . "/../view/partials/footer.php";
 ?>
