653278f414b86953ef7d62c26f564cfa7f1071ca
LukaszGolko <lukaszgolko26@gmail.com>
2024-08-22 22:01:24 +0200

    i have started doing languages

---
+++ languages/en.php
@@ -0,0 +1,13 @@
+<?php
+return [
+ 0 => "Name",
+ 1 => "Surname",
+ 2 => "Create",
+ 3 => "This user already exists!",
+ 4 => "This user has been created!",
+ 5 => "Search",
+ 6 => "There are no such records.",
+ 7 => "Record",
+ 8 => "has been deleted!",
+ 9 => "has been changed to"
+];
---
+++ languages/pl.php
@@ -0,0 +1,13 @@
+<?php
+return [
+ 0 => "Imię",
+ 1 => "Nazwisko",
+ 2 => "Stwórz",
+ 3 => "Ten użytkownik już istnieje!",
+ 4 => "Ten użytkownik został stworzony.",
+ 5 => "Szukaj",
+ 6 => "Nie ma takich rekordów!",
+ 7 => "Rekord",
+ 8 => "został usunięty!",
+ 9 => "został zmieniony na"
+];
